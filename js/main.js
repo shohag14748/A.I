3e104a1157fd552dd5723f22c032050b1467a458
Mohammad Ali Shohag <47399558+shohag14748@users.noreply.github.com>
2019-03-20 11:12:17 +0600

    upload

---
+++ js/main.js
@@ -0,0 +1,49 @@
+$(function () {
+    $("#wizard").steps({
+        headerTag: "h4",
+        bodyTag: "section",
+        transitionEffect: "fade",
+        enableAllSteps: true,
+        titleTemplate: '<span class="number">#index#</span>',
+        labels: {
+            current: "",
+            previous: '<i class="fas fa-angle-left"></i>',
+            next: '<i class="fas fa-angle-right"></i>',
+            finish: '<i class="fas fa-angle-right"></i>'
+        },
+        onStepChanging: function (event, currentIndex, newIndex) {
+//            if (newIndex >= 1) {
+//                $('.steps ul li:first-child a').addClass('active');
+////                $('.steps ul li:first-child a .step-arrow').remove();
+//            }
+////            else {
+////            }
+            if (newIndex === 1) {
+                $('.steps ul li:nth-child(2)').addClass('active');
+            } else {
+//                $('.steps ul li:nth-child(2) .step-arrow').remove();
+            }
+            if (newIndex === 2) {
+                $('.steps ul li:nth-child(3)').addClass('active');
+            } else {
+//                $('.steps ul li:nth-child(3) .step-arrow').remove();
+            }
+            if (newIndex === 3) {
+                $('.steps ul li:nth-child(4)').addClass('active');
+            } else {
+//                $('.steps ul li:nth-child(4) .step-arrow').remove();
+            }
+            return true;
+        }
+    });
+    
+    $('.actions ul li a').append('<i class="zmdi zmdi-chevron-left"></i>');
+    $('.steps ul li:first-child').addClass('active');
+    
+    
+    $('.toggle .icon').on('click',function(){
+        $('.header nav').slideToggle();
+    });
+    
+    
+})
